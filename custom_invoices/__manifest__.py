29c035ce840b290bdb3b513dae65a48e6a38d2bc
CrisCarrion505 <cristophercarrion1@gmail.com>
2025-10-09 09:47:13 -0500

    [ADD] filtered by provinces

---
+++ custom_invoices/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Custom Invoices",
+    "version": "17.0.1.0.0",
+    "summary": "Custom Invoices",
+    "depends": ["base","account"],
+    "data": [
+        "views/account_invoice_report_inherit.xml"
+    ],
+    "installable": True,
+    "application": True,    
+}
